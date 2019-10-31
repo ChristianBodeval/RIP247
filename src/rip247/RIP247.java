b3db13a40823ea2896fe5a0afb26287517d5ec73
Jeppe Stenstrup Lauridsen <jelau19@student.sdu.dk>
2019-10-31 10:00:19 +0100

    How dare you?
    How dare you?

--- src/rip247/RIP247.java
+++ src/rip247/RIP247.java
@@ -15,7 +15,7 @@ public class RIP247 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Ey");
+        System.out.println("You stole my childhood. How dare you?");
     }
     
 }
