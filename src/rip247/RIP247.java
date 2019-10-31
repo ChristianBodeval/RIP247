c25eec4404c4b1b943f3a661041474ac58695925
Nicolaj <nicni19@student.sdu.dk>
2019-10-31 09:57:45 +0100

    Tilføjet "NicosGren"

--- src/rip247/RIP247.java
+++ src/rip247/RIP247.java
@@ -16,6 +16,9 @@ public class RIP247 {
      */
     public static void main(String[] args) {
         System.out.println("Ey");
+        
+        
+        System.out.println("NicosGren");
     }
     
 }
